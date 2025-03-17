3aeee687548792a146bf9f67c1f188391968d718
VZstless <2319725508@qq.com>
2025-03-17 21:19:36 +0800

    added play_again1 & 2

---
+++ game/play_again/play_again2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+
+#define QUESTION "Do you want another transaction"
+
+int tty_mode(int);
+void set_cr_noecho_mode(void);
+int get_response(char *);
+
+int main()
+{
+	int response;
+	tty_mode(0);
+	set_cr_noecho_mode();
+	response = get_response(QUESTION);
+	tty_mode(1);
+	return response;
+}
+
+int tty_mode(int how)
+{
+	static struct termios original_mode;
+	if(how == 0)
+		tcgetattr(0, &original_mode);
+	else
+		return tcsetattr(0, TCSANOW, &original_mode);
+	return 0;
+}
+
+void set_cr_noecho_mode(void)
+{
+	struct termios ttystate;
+	tcgetattr(0, &ttystate);
+	ttystate.c_lflag &= ~ICANON;
+	ttystate.c_lflag &= ~ECHO;
+	ttystate.c_cc[VMIN] = 1;
+	tcsetattr(0, TCSANOW, &ttystate);
+}
+
+int get_response(char *question)
+{
+	printf("%s (y/n) ? ", question);
+	while(1)
+	{
+		switch(getchar())
+		{
+			case 'y':
+			case 'Y':
+				return 0;
+			case 'n':
+			case 'N':
+			case EOF:
+				return 1;
+		}
+	}
+}
