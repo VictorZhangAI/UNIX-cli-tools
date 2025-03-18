0964923bd06050df1ccd58e4d320fd098d8d7968
VZstless <2319725508@qq.com>
2025-03-18 19:51:04 +0800

    added bounce1d with headers

---
+++ game/bounce/ticker.c
@@ -0,0 +1,15 @@
+#include <sys/time.h>
+
+#include "ticker.h"
+
+int set_ticker(int n_msecs)
+{
+	struct itimerval new_timeset;
+	long n_sec, n_usecs;
+
+	n_sec = n_msecs / 1000;
+	n_usecs = (n_msecs % 1000) * 1000L;
+
+	new_timeset.it_interval.tv_sec = n_sec;
+	new_timeset.it_interval.tv_usec = n_usecs;
+}
---
+++ game/bounce/ticker.h
@@ -0,0 +1,6 @@
+#ifndef __TICKER_H__
+#define __TICKER_H__
+
+int set_ticker(int);
+
+#endif
