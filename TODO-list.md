687cbe6baea76a879b42fac1a341983f581fef12
VZstless <2319725508@qq.com>
2025-03-19 17:31:20 +0800

    finished bounce2d

--- TODO-list.md
+++ TODO-list.md
@@ -4,3 +4,5 @@ chapter2: ac, last, cat, head, tail, od, dd
 chapter3: chmod, chown, chgrp, file, finger, touch
 chapter4: find, du, ls -R, mount
 chapter5: write, stty, passwd, wall, biff, mt
+
+chapter7: snake, worms
--- game/bounce/ticker.c
+++ game/bounce/ticker.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <sys/time.h>
 
 #include "ticker.h"
@@ -12,4 +13,9 @@ int set_ticker(int n_msecs)
 
 	new_timeset.it_interval.tv_sec = n_sec;
 	new_timeset.it_interval.tv_usec = n_usecs;
+
+	new_timeset.it_value.tv_sec = n_sec;
+	new_timeset.it_value.tv_usec = n_usecs;
+
+	return setitimer(ITIMER_REAL, &new_timeset, NULL);
 }
